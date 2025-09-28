fa645ff975f29b0a2b52c7840c9326c7e1633682
joaoVitorLeal <joaoleal98@outlook.com>
2025-09-28 01:47:12 -0300

    feat: implement Product service contract

---
+++ products/src/main/java/io/github/joaoVitorLeal/marketsphere/products/service/impl/ProductServiceImpl.java
@@ -0,0 +1,50 @@
+package io.github.joaoVitorLeal.marketsphere.products.service.impl;
+
+import io.github.joaoVitorLeal.marketsphere.products.exception.ProductNotFoundException;
+import io.github.joaoVitorLeal.marketsphere.products.dto.ProductRequestDto;
+import io.github.joaoVitorLeal.marketsphere.products.dto.ProductResponseDto;
+import io.github.joaoVitorLeal.marketsphere.products.model.Product;
+import io.github.joaoVitorLeal.marketsphere.products.repository.ProductRepository;
+import io.github.joaoVitorLeal.marketsphere.products.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository repository;
+
+    @Transactional
+    @Override
+    public ProductResponseDto create(ProductRequestDto dto) {
+        Product product = new Product(dto.name(), dto.unitPrice(), dto.description());
+        repository.save(product);
+        return new ProductResponseDto(product.getId(), product.getName(), product.getUnitPrice(), product.getDescription());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public ProductResponseDto getProductById(Long id) {
+        Product product = repository.findById(id)
+                .orElseThrow( ()-> new ProductNotFoundException(id) );
+        return new ProductResponseDto(product.getId(), product.getName(), product.getUnitPrice(), product.getDescription());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ProductResponseDto> getAllProducts() {
+        return repository.findAll()
+                .stream()
+                .map( product -> new ProductResponseDto(
+                        product.getId(),
+                        product.getName(),
+                        product.getUnitPrice(),
+                        product.getDescription()
+                ))
+                .toList();
+    }
+}
