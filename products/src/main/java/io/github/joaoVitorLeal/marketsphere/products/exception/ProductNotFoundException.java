e49c376ec023ddf18eacabae704253ba7c156ca1
joaoVitorLeal <joaoleal98@outlook.com>
2025-09-28 01:50:19 -0300

    feat(product): add exception of Product not found

---
+++ products/src/main/java/io/github/joaoVitorLeal/marketsphere/products/exception/ProductNotFoundException.java
@@ -0,0 +1,20 @@
+package io.github.joaoVitorLeal.marketsphere.products.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException() {
+        super("Product not found.");
+    }
+
+    public ProductNotFoundException(Long id) {
+        super("Product not found with ID: " + id);
+    }
+
+    public ProductNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
