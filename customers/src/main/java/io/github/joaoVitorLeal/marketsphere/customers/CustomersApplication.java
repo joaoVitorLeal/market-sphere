a64439522de0f000d4a6301272acdf1307645dd4
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-01 00:51:43 -0300

    feat(customers): Enable OpenFeing for HTTP clients of the application

--- customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/CustomersApplication.java
+++ customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/CustomersApplication.java
@@ -1,9 +1,15 @@
 package io.github.joaoVitorLeal.marketsphere.customers;
 
+import io.github.joaoVitorLeal.marketsphere.customers.client.BrasilApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(
+        basePackages = "io.github.joaoVitorLeal.marketsphere.customers.client",
+        clients = BrasilApi.class
+)
 public class CustomersApplication {
 
 	public static void main(String[] args) {
