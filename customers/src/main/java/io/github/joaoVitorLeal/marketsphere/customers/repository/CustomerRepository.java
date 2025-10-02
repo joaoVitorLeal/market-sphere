0324cf206ad6c3ac1fccf37b751f951638db09e2
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-01 23:13:12 -0300

    feat(customers): add CustomerRepository#existsByEmail and CustomerRepository#existsByNationalId for validations

--- customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/repository/CustomerRepository.java
+++ customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/repository/CustomerRepository.java
@@ -4,4 +4,7 @@ import io.github.joaoVitorLeal.marketsphere.customers.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    boolean existsByEmail(String email);
+    boolean existsByNationalId(String nationalId);
 }
