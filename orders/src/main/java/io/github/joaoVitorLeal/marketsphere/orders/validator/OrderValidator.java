cc86e7048031cee4f2bcd092b68e79d1e550ad33
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-02 16:19:10 -0300

    feat(orders): add initial OrderValidator

---
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/validator/OrderValidator.java
@@ -0,0 +1,15 @@
+package io.github.joaoVitorLeal.marketsphere.orders.validator;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderValidator {
+
+    public void validate() {
+
+    }
+}
