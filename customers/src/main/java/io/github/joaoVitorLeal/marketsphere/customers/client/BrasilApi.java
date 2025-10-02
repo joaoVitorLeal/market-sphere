84220f3d5d46198c69fb8db495588effa5f528dc
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-01 23:07:18 -0300

    feat(customers): integrate BrasilApi#getAddressDataByPostalCode

--- customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/client/BrasilApi.java
+++ customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/client/BrasilApi.java
@@ -1,6 +1,11 @@
 package io.github.joaoVitorLeal.marketsphere.customers.client;
 
+import io.github.joaoVitorLeal.marketsphere.customers.client.config.BrasilApiFeignConfig;
+import io.github.joaoVitorLeal.marketsphere.customers.dto.brasilapi.BrasilApiAddressDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "brasilapi",
@@ -8,5 +13,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 )
 public interface BrasilApi {
 
-
+    @GetMapping(
+            value = "/cep/v1/{brazilianPostalCode}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    BrasilApiAddressDto getAddressDataByPostalCode(@PathVariable String brazilianPostalCode);
 }
