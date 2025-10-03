5d00b76558e6cebb3e67834e8e8cb536736e7343
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-03 20:06:53 -0300

    feat(orders): add POST endpoint in OrderController

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/controller/OrderController.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/controller/OrderController.java
@@ -1,7 +1,14 @@
 package io.github.joaoVitorLeal.marketsphere.orders.controller;
 
+import io.github.joaoVitorLeal.marketsphere.orders.controller.util.HeaderLocationBuilder;
+import io.github.joaoVitorLeal.marketsphere.orders.dto.OrderRequestDto;
 import io.github.joaoVitorLeal.marketsphere.orders.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService service;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createOrder(@RequestBody @Valid OrderRequestDto orderRequestDto) {
+        return ResponseEntity
+                .created(HeaderLocationBuilder.build(service.createOrder(orderRequestDto).id()))
+                .build();
+    }
 }
