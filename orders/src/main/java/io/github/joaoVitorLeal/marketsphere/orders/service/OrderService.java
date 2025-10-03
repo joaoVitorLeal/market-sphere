ab639a0cf9bde728e6cc0e13bc70065b0a23bcf1
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-03 19:46:57 -0300

    feat(orders): add OrderService#createOrder method

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/service/OrderService.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/service/OrderService.java
@@ -1,6 +1,9 @@
 package io.github.joaoVitorLeal.marketsphere.orders.service;
 
+import io.github.joaoVitorLeal.marketsphere.orders.dto.OrderRequestDto;
+import io.github.joaoVitorLeal.marketsphere.orders.dto.OrderResponseDto;
+
 public interface OrderService {
 
-
+    OrderResponseDto createOrder(OrderRequestDto orderRequestDto);
 }
