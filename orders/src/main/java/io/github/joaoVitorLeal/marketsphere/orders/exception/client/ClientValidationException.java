bfb3c14dbb0c41fdce4dd8339cfd23658c7c1571
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-03 23:56:50 -0300

    feat(orders): introduce ClientValidationException for HTTP client error responses

---
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/exception/client/ClientValidationException.java
@@ -0,0 +1,24 @@
+package io.github.joaoVitorLeal.marketsphere.orders.exception.client;
+
+import lombok.Getter;
+
+@Getter
+public abstract class ClientValidationException extends RuntimeException {
+
+    private final String field;
+
+    public ClientValidationException() {
+        super();
+        field = null;
+    }
+
+    public ClientValidationException(String field, String message) {
+        super(message);
+        this.field = field;
+    }
+
+    public ClientValidationException(String message, Throwable cause) {
+        super(message, cause);
+        this.field = null;
+    }
+}
