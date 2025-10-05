38106330c35da0a5e81ba3456170a52855f22311
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-04 22:06:30 -0300

    feat(orders): implement OrderServiceImpl#createOrder and OrderServiceImpl#getOrderById

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/service/impl/OrderServiceImpl.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/service/impl/OrderServiceImpl.java
@@ -4,8 +4,12 @@ import io.github.joaoVitorLeal.marketsphere.orders.client.banking.BankingClient;
 import io.github.joaoVitorLeal.marketsphere.orders.dto.OrderRequestDto;
 import io.github.joaoVitorLeal.marketsphere.orders.dto.OrderResponseDto;
 import io.github.joaoVitorLeal.marketsphere.orders.client.banking.representation.BankingPaymentRepresentation;
+import io.github.joaoVitorLeal.marketsphere.orders.exception.OrderNotFoundException;
 import io.github.joaoVitorLeal.marketsphere.orders.mapper.OrderMapper;
 import io.github.joaoVitorLeal.marketsphere.orders.model.Order;
+import io.github.joaoVitorLeal.marketsphere.orders.model.PaymentInfo;
+import io.github.joaoVitorLeal.marketsphere.orders.model.enums.OrderStatus;
+import io.github.joaoVitorLeal.marketsphere.orders.model.enums.PaymentType;
 import io.github.joaoVitorLeal.marketsphere.orders.repository.OrderItemRepository;
 import io.github.joaoVitorLeal.marketsphere.orders.repository.OrderRepository;
 import io.github.joaoVitorLeal.marketsphere.orders.service.OrderService;
@@ -17,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private static final String CREATED_NEW_PAYMENT_OBSERVATION_MESSAGE = "New payment made. Awaiting processing.";
 
     private final OrderRepository repository;
     private final OrderItemRepository orderItemRepository;
@@ -32,6 +38,30 @@ public class OrderServiceImpl implements OrderService {
 
         sendPaymentRequest(createdOrder);
         return new OrderResponseDto(createdOrder.getId());
+    }
+
+    @Transactional
+    @Override
+    public void createPayment(Long orderId, String metadata, PaymentType paymentType) {
+        Order existingOrder = repository.findById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException(orderId));
+
+        PaymentInfo paymentInfo = new PaymentInfo();
+        paymentInfo.setPaymentType(paymentType);
+        paymentInfo.setMetadata(metadata);
+        existingOrder.setStatus(OrderStatus.PLACED);
+        existingOrder.setObservations(CREATED_NEW_PAYMENT_OBSERVATION_MESSAGE);
+
+        BankingPaymentRepresentation bankingPaymentRepresentation = bankingClient.requestPayment(existingOrder);
+        existingOrder.setPaymentKey(bankingPaymentRepresentation.paymentKey());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public OrderResponseDto getOrderById(Long orderId) {
+        return repository.findById(orderId)
+                .map(mapper::toOrderDto)
+                .orElseThrow(() -> new OrderNotFoundException(orderId));
     }
 
     private void sendPaymentRequest(Order createdOrder) {
