e737a8aa7819f2452b13c0a374cfea08da3f2670
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-06 11:55:24 -0300

    refactor(orders): declare ignored attributes explicitly

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/mapper/OrderItemMapper.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/mapper/OrderItemMapper.java
@@ -3,9 +3,12 @@ package io.github.joaoVitorLeal.marketsphere.orders.mapper;
 import io.github.joaoVitorLeal.marketsphere.orders.dto.OrderItemRequestDto;
 import io.github.joaoVitorLeal.marketsphere.orders.model.OrderItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderItemMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "order", ignore = true)
     OrderItem toOrderItemEntity(OrderItemRequestDto orderItemRequestDto);
 }
