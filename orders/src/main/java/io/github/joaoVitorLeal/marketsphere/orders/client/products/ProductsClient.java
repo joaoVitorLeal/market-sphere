ef1f2ea6a93138b5364c2828536aa1def781aa67
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-06 12:04:30 -0300

    feat(orders): add ProductsClient#getAllProductsByIds

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/client/products/ProductsClient.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/client/products/ProductsClient.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient(
         name = "products",
@@ -14,4 +17,7 @@ public interface ProductsClient {
 
     @GetMapping("/{productId}")
     ResponseEntity<ProductRepresentation> getProductById(@PathVariable Long productId);
+
+    @GetMapping()
+    ResponseEntity<List<ProductRepresentation>> getAllProductsByIds(@RequestParam("productsIds") List<Long> productsIds);
 }
