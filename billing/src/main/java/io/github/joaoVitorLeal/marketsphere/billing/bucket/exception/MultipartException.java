eef16e4e2148aeb3f76f9d718e8b086aa191764b
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-21 16:29:55 -0300

    feat(billing): add custom exceptions for invoice generation, messaging, and order processing

---
+++ billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/bucket/exception/MultipartException.java
@@ -0,0 +1,8 @@
+package io.github.joaoVitorLeal.marketsphere.billing.bucket.exception;
+
+public class MultipartException extends RuntimeException {
+
+    public MultipartException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/bucket/exception/StorageAccessException.java
@@ -0,0 +1,8 @@
+package io.github.joaoVitorLeal.marketsphere.billing.bucket.exception;
+
+public class StorageAccessException extends RuntimeException {
+
+    public StorageAccessException(String message, Throwable cause) {
+        super(message);
+    }
+}
