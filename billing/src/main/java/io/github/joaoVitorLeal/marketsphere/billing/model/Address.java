5601b046b03e66f8bec2fdc16a5ba1a070a320b3
joaoVitorLeal <joaoleal98@outlook.com>
2025-10-21 16:22:59 -0300

    feat(billing): add model classes Address, Customer, Order, OrderItem

---
+++ billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/model/Address.java
@@ -0,0 +1,12 @@
+package io.github.joaoVitorLeal.marketsphere.billing.model;
+
+public record Address(
+        String postalCode,
+        String street,
+        String number,
+        String complement,
+        String neighborhood,
+        String city,
+        String state,
+        String country
+) { }
