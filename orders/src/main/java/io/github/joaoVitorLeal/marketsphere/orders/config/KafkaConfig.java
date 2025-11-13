0b333dd7f44bc456ca0f8ac0fa779a2d5e051415
joaoVitorLeal <joaoleal98@outlook.com>
2025-11-12 21:31:25 -0300

    feat(order): add Kafka consumer factory and its configuration

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/config/KafkaConfig.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/config/KafkaConfig.java
@@ -1,13 +1,14 @@
 package io.github.joaoVitorLeal.marketsphere.orders.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +19,9 @@ public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    /**
+     * Configurações do Producer
+     * */
     // Configurando propriedades do produtor das mensagens | <String, String> Tipo de dado que iremos trafegar no Kafka
     @Bean
     public ProducerFactory<String, String> producerFactory() {
@@ -32,5 +36,27 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    /**
+     * Configurações do Consumer
+     * */
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);   // Definir o tipo de classe que irá configurar a desserialização das chaves
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // Definir o tipo de classe que irá configurar a desserialização dos valores
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    // Definir como as mensagens do broker serão consumidas
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> listener = new ConcurrentKafkaListenerContainerFactory<>();
+        listener.setConsumerFactory(consumerFactory);
+        return listener;
     }
 }
