618dbcf60ac0cd26191ed88e8704e7cadc2e7c37
joaoVitorLeal <joaoleal98@outlook.com>
2025-11-16 21:41:07 -0300

    refactor(orders): remove unused constructors

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/exception/client/products/ProductClientNotFoundException.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/exception/client/products/ProductClientNotFoundException.java
@@ -3,16 +3,7 @@ package io.github.joaoVitorLeal.marketsphere.orders.exception.client.products;
 import io.github.joaoVitorLeal.marketsphere.orders.exception.client.ClientValidationException;
 
 public final class ProductClientNotFoundException extends ClientValidationException {
-
-    public ProductClientNotFoundException() {
-        super();
-    }
-
     public ProductClientNotFoundException(String field, String message) {
         super(field, message);
-    }
-
-    public ProductClientNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
