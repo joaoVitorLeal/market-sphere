94b457b6dff4208bad9dfa8ecec0c017a95e7ddc
joaoVitorLeal <joaoleal98@outlook.com>
2025-11-16 23:35:35 -0300

    feat(marketsphere-services): update database schema

--- marketsphere-services/database/schema.sql
+++ marketsphere-services/database/schema.sql
@@ -1,10 +1,16 @@
--- table of customers
+--- Bancos de dados
+create database market_sphere_products;
+create database market_sphere_orders;
+create database market_sphere_customers;
+
+-- Tabela customers
 create table customers (
     id serial not null,
     full_name varchar(200) not null,
     national_id varchar(20) not null,
     email varchar(150) not null,
     phone_number varchar(25) not null,
+    active boolean not null default true,
 
     -- Embedded Address (VO)
     postal_code varchar(20) not null,
@@ -21,7 +27,6 @@ create table customers (
     constraint uq_customers_email unique (email)
 );
 
--- Comments for better documentation
 comment on table customers is 'Customers master data, including embedded Address value object';
 
 comment on column customers.id is 'Primary key (surrogate)';
@@ -40,40 +45,58 @@ comment on column customers.city is 'Customer''s city';
 comment on column customers.state is 'Customer''s state, province or region';
 comment on column customers.country is 'Customer''s country';
 
-
-
--- table of products
+-- Tabela: products
 create table products (
-    id serial not null primary key,
+    id serial not null,
     name varchar(150) not null,
     unit_price decimal(16,2) not null,
-    description text not null
+    description text not null,
+    active boolean not null default true,
+
+    constraint pk_products_id primary key (id)
 );
 
 comment on column products.id is 'Unique identifier for the product';
 comment on column products.name is 'Name of the product';
 comment on column products.unit_price is 'Price of a single unit of the product';
 comment on column products.description is 'Detailed description of the product';
+comment on column products.active is 'Define if product is active (available)';
 
 
--- table of orders
+-- Tabela: orders
 create table orders (
 	id bigserial not null,
 	customer_id bigint not null,
-	order_date timestamp not null default now(),
+	order_date timestamp with time zone not null default now(),
+	paid_at timestamp with time zone null,
+	billed_at timestamp with time zone null,
+	shipped_at timestamp with time zone null,
 	payment_key text,
 	observations text,
 	status varchar(30),
-	total decimal (16,2) not null,
+	total decimal(16,2) not null,
 	tracking_code uuid,
 	invoice_url text,
 
 	constraint pk_orders_id primary key (id),
 	constraint chk_orders_status check (
-        status in ('PLACED', 'PAID', 'BILLED', 'SHIPPED', 'PAYMENT_ERROR', 'PREPARING_SHIPMENT')
+        status in ('PLACED', 'PAID', 'BILLED', 'SHIPPED', 'PAYMENT_ERROR', 'PREPARING_SHIPMENT', 'CANCELED')
     )
 );
 
+comment on table orders is 'stores customer orders';
+comment on column orders.id is 'primary key for orders';
+comment on column orders.customer_id is 'reference to the customer who placed the order';
+comment on column orders.order_date is 'timestamp (utc) recording the exact moment the order was created.';
+comment on column orders.paid_at is 'timestamp (utc) recording the exact moment the order payment was successfully confirmed.';
+comment on column orders.billed_at is 'timestamp (utc) recording the exact moment the order invoice was successfully generated.';
+comment on column orders.shipped_at is 'timestamp (utc) recording the exact moment the order was dispatched for delivery (shipped).';
+comment on column orders.payment_key is 'identifier for the payment transaction';
+comment on column orders.observations is 'additional notes or comments about the order';
+comment on column orders.status is 'current status of the order';
+comment on column orders.total is 'total value of the order';
+comment on column orders.tracking_code is 'shipping tracking code';
+comment on column orders.invoice_url is 'url link to the invoice';
 comment on table orders is 'Stores customer orders';
 comment on column orders.id is 'Primary key for orders';
 comment on column orders.customer_id is 'Reference to the customer who placed the order';
@@ -86,7 +109,7 @@ comment on column orders.tracking_code is 'Shipping tracking code';
 comment on column orders.invoice_url is 'URL link to the invoice';
 
 
--- table of order items
+-- Tabela: order_items
 create table order_items (
 	id bigserial not null,
 	order_id bigint not null ,
