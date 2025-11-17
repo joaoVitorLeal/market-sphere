73ea67bf6ab4553e80535eae8327267e956b7f64
joaoVitorLeal <joaoleal98@outlook.com>
2025-11-16 21:19:06 -0300

    feat(billing): improve bucket upload handling and return 201 Created with Location header

--- billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/api/BucketController.java
+++ billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/api/BucketController.java
@@ -8,10 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Objects;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/bucket")
@@ -28,10 +29,26 @@ public class BucketController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile multipartFile) {
         try (InputStream inputStream = multipartFile.getInputStream()) {
-            MediaType mediaType = MediaType.parseMediaType(Objects.requireNonNull(multipartFile.getContentType()));
-            service.upload(new BucketFile(multipartFile.getOriginalFilename(), inputStream, mediaType, multipartFile.getSize()));
+
+            String contentType = multipartFile.getContentType();
+            MediaType mediaType;
+            if (contentType == null || contentType.isBlank()) {
+                // Define o tipo como arquivo binário genérico se o tipo não for informado
+                mediaType = MediaType.APPLICATION_OCTET_STREAM;
+            } else {
+                mediaType = MediaType.parseMediaType(contentType);
+            }
+
+            service.upload(new BucketFile(
+                    multipartFile.getOriginalFilename(),
+                    inputStream, mediaType,
+                    multipartFile.getSize()
+            ));
+
+            URI location = this.generateHeaderLocation(multipartFile.getOriginalFilename());
             String successMessage = translator.translate("file.upload.successful");
-            return ResponseEntity.ok(successMessage);
+            return ResponseEntity.created(location).body(successMessage);
+
         } catch (IOException e) {
             throw new StorageAccessException("Could not read input stream from uploaded multipartFile", e);
         }
@@ -41,5 +58,13 @@ public class BucketController {
     public ResponseEntity<String> getFileUrl(@PathVariable String fileName) {
         String fileUrl = service.generatePresignedUrl(fileName);
         return ResponseEntity.ok(fileUrl);
+    }
+
+    private URI generateHeaderLocation(String fileName) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{filename}")
+                .buildAndExpand(fileName)
+                .toUri();
     }
 }
