933d3480814ad33c4737f35d3c2359b1046f19ad
joaoVitorLeal <joaoleal98@outlook.com>
2025-11-16 21:18:16 -0300

    feat(billing): enable Async configuration

--- billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/BillingApplication.java
+++ billing/src/main/java/io/github/joaoVitorLeal/marketsphere/billing/BillingApplication.java
@@ -2,8 +2,10 @@ package io.github.joaoVitorLeal.marketsphere.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BillingApplication {
 
 	public static void main(String[] args) {
