5a4965caf329e0f8c741d778f0de2b70d3252f49
joaoVitorLeal <joaoleal98@outlook.com>
2025-12-10 17:17:39 -0300

    feat(orders): add endpoint for fetching customers ignoring soft delete filter and enable Feign config

--- orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/client/customers/CustomersClient.java
+++ orders/src/main/java/io/github/joaoVitorLeal/marketsphere/orders/client/customers/CustomersClient.java
@@ -1,6 +1,7 @@
 package io.github.joaoVitorLeal.marketsphere.orders.client.customers;
 
 import io.github.joaoVitorLeal.marketsphere.orders.client.customers.representation.CustomerRepresentation;
+import io.github.joaoVitorLeal.marketsphere.orders.config.CustomersClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "customers",
-        url = "${market-sphere.feign.clients.customers.base-url}"
+        url = "${market-sphere.feign.clients.customers.base-url}",
+        configuration = CustomersClientConfig.class
 )
 public interface CustomersClient {
 
     @GetMapping("/{customerId}")
     ResponseEntity<CustomerRepresentation> getCustomerById(@PathVariable Long customerId);
+
+    @GetMapping("/for-orders-service/{customerId}")
+    ResponseEntity<CustomerRepresentation> getCustomerByIdIgnoringFilter(@PathVariable Long customerId);
 }
