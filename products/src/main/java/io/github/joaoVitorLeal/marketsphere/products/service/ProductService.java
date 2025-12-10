e741a36aa95a38149c1e56872baaa698d1edbe62
joaoVitorLeal <joaoleal98@outlook.com>
2025-12-10 17:41:09 -0300

    feat(products): adapt ProductService to support soft delete and retrieval ignoring active filter

--- products/src/main/java/io/github/joaoVitorLeal/marketsphere/products/service/ProductService.java
+++ products/src/main/java/io/github/joaoVitorLeal/marketsphere/products/service/ProductService.java
@@ -6,8 +6,28 @@ import io.github.joaoVitorLeal.marketsphere.products.dto.ProductResponseDto;
 import java.util.List;
 
 public interface ProductService {
+
     ProductResponseDto createProduct(ProductRequestDto productRequestDto);
+
     ProductResponseDto getProductById(Long productId);
+
     List<ProductResponseDto> getAllProducts();
-    List<ProductResponseDto> getAllProductsByIds(List<Long> productsIds);
+
+    /**
+     * Busca uma lista de produtos <strong>ativos</strong> e <strong>inativos</strong> por uma lista de IDs.
+     * @param productsIds IDs dos produtos
+     */
+    List<ProductResponseDto> getAllProductsByIdsIgnoringFilter(List<Long> productsIds);
+
+    /**
+     * Realiza a exclusão lógica de um produto
+     * @param productId ID do produto a ser inativado
+     * */
+    void deleteProductById(Long productId);
+
+    /**
+     * Reativa um produto que foi logicamente excluído.
+     * @param productId O ID do produto a ser reativado.
+     */
+    void reactivateProductById(Long productId);
 }
