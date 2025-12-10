da67c21f7a82c51faa1ca8a532c6cb55da09624f
joaoVitorLeal <joaoleal98@outlook.com>
2025-12-10 17:14:01 -0300

    refactor(customers): simplify CustomerNotFoundException constructors

--- customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/exception/CustomerNotFoundException.java
+++ customers/src/main/java/io/github/joaoVitorLeal/marketsphere/customers/exception/CustomerNotFoundException.java
@@ -5,16 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException {
-
-    public CustomerNotFoundException() {
-        super("Customer not found.");
+    public CustomerNotFoundException(String message) {
+        super(message);
     }
 
     public CustomerNotFoundException(Long customerId) {
         super("Customer not found with ID: " + customerId);
-    }
-
-    public CustomerNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
